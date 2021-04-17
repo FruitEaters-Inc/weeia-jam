de42350ace54965977d145744b385f1144371f54
Daniel Wieczorek <222606@edu.p.lodz.pl>
2021-04-17 19:34:59 +0200

    Improved file handling

--- game/environment.py
+++ game/environment.py
@@ -7,6 +7,7 @@ WIDTH = 20
 HEIGHT = 20
 SPRITE_SIZE = 32
 
+
 class TileType(Enum):
     EMPTY = 1
     WALL = 2
@@ -31,31 +32,25 @@ TILE_DICT = {
 
 
 class Tile:
-    def __init__(self, tileType, tileSprite):
+    def __init__(self, tileType, fileName):
         self.type = tileType
-        self.sprite = tileSprite
-
-
-class FileHandler:
-    def __init__(self, path):
-        self.height = HEIGHT
-        self.width = WIDTH
-        self.tileMatrix = [[Tile(TileType.WALL, 'crate.png') for i in range(0, self.width)] for x in
-                           range(0, self.height)]
-        with (open(path)) as file:
-            for yIndex, line in enumerate(file):
-                for xIndex, ch in enumerate(line):
-                    self.tileMatrix[yIndex][xIndex] = Tile(TILE_DICT[ch])
 
 
 class Environment:
-    def __init__(self):
+    def __init__(self, path):
         self.height = HEIGHT
         self.width = WIDTH
         self.sprite_size = SPRITE_SIZE
         self.tileMatrix = [[Tile(TileType.WALL, 'wall.png') for i in range(0, self.width)] for x in
-                               range(0, self.height)]
-        self.CRATE_IMAGE = pygame.image.load(os.path.join('game', 'Assets', 'crate.png'))
+                           range(0, self.height)]
+
+        self.openFile(path)
+
+    def openFile(self, path):
+        with (open(path)) as file:
+            for yIndex, line in enumerate(file):
+                for xIndex, ch in enumerate(line):
+                    self.tileMatrix[yIndex][xIndex] = Tile(*TILE_DICT[ch])
 
     def print(self):
         for y in range(self.height):
@@ -78,7 +73,7 @@ class Environment:
         print("Todo")
 
     def checkPlayerMove(self, expX, expY):
-        if expX >= self.width | expY >= self.height | self.tileTypeMatrix[expY][expX] != TileType.EMPTY:
+        if expX >= self.width | expY >= self.height | self.tileMatrix[expY][expX].type != TileType.EMPTY:
             return
         else:
             Environment.movePlayer()
