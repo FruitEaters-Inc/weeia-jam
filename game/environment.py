305693b31d94a4cbad1865deb5069838d7a85e4e
Daniel Wieczorek <222606@edu.p.lodz.pl>
2021-04-17 19:22:48 +0200

    Added new consts, working on file handling

--- game/environment.py
+++ game/environment.py
@@ -3,6 +3,9 @@ import os
 from game import *
 import pygame
 
+WIDTH = 20
+HEIGHT = 20
+SPRITE_SIZE = 32
 
 class TileType(Enum):
     EMPTY = 1
@@ -11,6 +14,7 @@ class TileType(Enum):
     BORDER = 4
     CRATE = 5
 
+
 class Direction(Enum):
     UP = 1
     LEFT = 2
@@ -18,15 +22,40 @@ class Direction(Enum):
     RIGHT = 4
 
 
+TILE_DICT = {
+    '#': [TileType.WALL, 'wall.png'],
+    '_': [TileType.EMPTY, 'empty.png'],
+    'X': [TileType.CRATE, 'crate.png'],
+    '|': [TileType.BORDER, 'border.png']
+}
+
+
+class Tile:
+    def __init__(self, tileType, tileSprite):
+        self.type = tileType
+        self.sprite = tileSprite
+
+
+class FileHandler:
+    def __init__(self, path):
+        self.height = HEIGHT
+        self.width = WIDTH
+        self.tileMatrix = [[Tile(TileType.WALL, 'crate.png') for i in range(0, self.width)] for x in
+                           range(0, self.height)]
+        with (open(path)) as file:
+            for yIndex, line in enumerate(file):
+                for xIndex, ch in enumerate(line):
+                    self.tileMatrix[yIndex][xIndex] = Tile(TILE_DICT[ch])
+
+
 class Environment:
     def __init__(self):
-        self.height = 20
-        self.width = 20
-        self.sprite_size = 32
-        self.matrix = [['#' for i in range(0, self.width)] for x in range(0, self.height)]
-        self.tileTypeMatrix = [[5 for i in range(0, self.width)] for x in range(0, self.height)]
+        self.height = HEIGHT
+        self.width = WIDTH
+        self.sprite_size = SPRITE_SIZE
+        self.tileMatrix = [[Tile(TileType.WALL, 'wall.png') for i in range(0, self.width)] for x in
+                               range(0, self.height)]
         self.CRATE_IMAGE = pygame.image.load(os.path.join('game', 'Assets', 'crate.png'))
-
 
     def print(self):
         for y in range(self.height):
@@ -38,8 +67,8 @@ class Environment:
         for y in range(self.height):
             for x in range(self.width):
                 if self.tileTypeMatrix[y][x]:
-                    winObj.blit(self.CRATE_IMAGE,(x * self.sprite_size, y * self.sprite_size))
-        
+                    winObj.blit(self.CRATE_IMAGE, (x * self.sprite_size, y * self.sprite_size))
+
         pygame.display.update()
 
     #   def identifyField(self, x, y):
