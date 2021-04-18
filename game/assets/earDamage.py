66741a49918dcf97cd8b0f0f11ee2f52be0c4885
Daniel Wieczorek <222606@edu.p.lodz.pl>
2021-04-18 04:14:50 +0200

    uszkodziło mózg

--- game/assets/earDamage.py
+++ game/assets/earDamage.py
@@ -1,1 +1,12 @@
-messagePool = []
+messagePool = [
+    "aktywuj_system_windows.wav",
+    "kiedy_bylo_jutro.wav",
+    "kim_jestes.wav",
+    "teraz_wszystko_mnie_boli.wav",
+    "tur.wav",
+    "UNAUTHORIZED.wav",
+    "wlacz_myslenie.wav",
+    "wylacz_telewizje.wav",
+    "zachodzace_slonce",
+    "zdejmij_kaganiec.wav"
+]
